b216a24ede439e458825c2e192e9b0e1f9fc2a46
Buddha Saikia <bsaikia.dev@gmail.com>
2026-03-05 00:14:31 +0530

    Add registration endpoint and form model
    Introduce a POST /register route that accepts a RegistrationForm, logs the payload, and responds with HTTP 201 Created. Add a new RegistrationForm data class (username, email) to represent the incoming registration payload. This enables basic user registration handling on the server.

--- src/main/kotlin/com/bs/basicktorserver/Application.kt
+++ src/main/kotlin/com/bs/basicktorserver/Application.kt
@@ -1,11 +1,14 @@
 package com.bs.basicktorserver
 
 import com.bs.basicktorserver.model.Profile
+import com.bs.basicktorserver.model.RegistrationForm
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.gson.gson
 import io.ktor.server.application.install
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -28,6 +31,11 @@ fun main() {
                 val myProfile = Profile("Alex", 25)
                 // Respond with the profile data as JSON
                 call.respond(myProfile)
+            }
+            post("/register") {
+                val from = call.receive<RegistrationForm>()
+                println("Received registration form: $from")
+                call.respond(HttpStatusCode.Created, "Registration successful for ${from.username}")
             }
         }
     }.start(wait = true)
